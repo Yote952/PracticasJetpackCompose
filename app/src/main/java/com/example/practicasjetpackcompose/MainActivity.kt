c8960ccaf1ddfeeeececc4146f17ea5df69430cc
Yote952 <111202046+Yote952@users.noreply.github.com>
2024-06-17 15:08:54 -0600

    Practica de State

--- app/src/main/java/com/example/practicasjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/practicasjetpackcompose/MainActivity.kt
@@ -15,20 +15,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.practicasjetpackcompose.ui.theme.PracticasJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +40,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticasJetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyComplexLayout(
+                    MyStateExample(
                             modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -46,139 +48,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@Composable
-fun MyConstraintLayout(){
-    ConstraintLayout(modifier = Modifier.fillMaxSize()){
-        val (boxRed, boxBlue,boxYellow, boxMagenta, boxGreen) = createRefs()
-        Box(
-            modifier = Modifier
-                .size(125.dp)
-                .background(Color.Red)
-                .constrainAs(boxRed) {
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                }
-        ){
 
-        }
-        Box(
-            modifier = Modifier
-                .size(125.dp)
-                .background(Color.Blue)
-                .constrainAs(boxBlue) {
-                    top.linkTo(boxRed.bottom)
-                    start.linkTo(boxRed.end)
-                }
-        ){
-
-        }
-        Box(
-            modifier = Modifier
-                .size(125.dp)
-                .background(Color.Green)
-                .constrainAs(boxGreen) {
-                    top.linkTo(boxRed.bottom)
-                    end.linkTo(boxRed.start)
-                }
-        ){
-
-        }
-        Box(
-            modifier = Modifier
-                .size(125.dp)
-                .background(Color.Yellow)
-                .constrainAs(boxYellow) {
-                    bottom.linkTo(boxRed.top)
-                    end.linkTo(boxRed.start)
-                }
-        ){
-
-        }
-        Box(
-            modifier = Modifier
-                .size(125.dp)
-                .background(Color.Magenta)
-                .constrainAs(boxMagenta) {
-                    bottom.linkTo(boxRed.top)
-                    start.linkTo(boxRed.end)
-                }
-        ){
-
-        }
-    }
-}
-@Composable
-fun ConstraintExampleGuide(){
-        ConstraintLayout(Modifier.fillMaxSize()) {
-            val boxRed = createRef()
-            val topGuide = createGuidelineFromTop(0.1f)
-            val startGuide = createGuidelineFromStart(0.25f)
-            Box(modifier = Modifier
-                .size(125.dp)
-                .background(Color.Red)
-                .constrainAs(boxRed) {
-                    top.linkTo(topGuide)
-                    start.linkTo(startGuide)
-                })
-        }
-}
-@Composable
-fun ConstraintExampleBarrier(){
-    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        val (boxRed, boxGreen, boxYellow) = createRefs()
-        val barrier = createEndBarrier(boxRed,boxGreen)
-        Box(modifier = Modifier
-            .size(125.dp)
-            .background(Color.Green)
-            .constrainAs(boxGreen) {
-                start.linkTo(parent.start, margin = 16.dp)
-            })
-        Box(modifier = Modifier
-            .size(235.dp)
-            .background(Color.Red)
-            .constrainAs(boxRed) {
-                top.linkTo(boxGreen.bottom)
-                start.linkTo(parent.start, margin = 32.dp)
-            })
-        Box(modifier = Modifier
-            .size(50.dp)
-            .background(Color.Yellow)
-            .constrainAs(boxYellow) {
-                start.linkTo(barrier)
-            })
-    }
-}
-@Preview
-@Composable
-fun ConstraintChainExample(){
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        val (boxRed, boxGreen, boxYellow) = createRefs()
-        Box(modifier = Modifier
-            .size(75.dp)
-            .background(Color.Green)
-            .constrainAs(boxGreen) {
-                start.linkTo(parent.start)
-                end.linkTo(boxRed.start)
-            })
-        Box(modifier = Modifier
-            .size(75.dp)
-            .background(Color.Red)
-            .constrainAs(boxRed) {
-                start.linkTo(boxGreen.end)
-                end.linkTo(boxYellow.start)
-            })
-        Box(modifier = Modifier
-            .size(75.dp)
-            .background(Color.Yellow)
-            .constrainAs(boxYellow) {
-                start.linkTo(boxRed.end)
-                end.linkTo(parent.end)
-            })
-        createHorizontalChain(boxRed,boxGreen,boxYellow, chainStyle = ChainStyle.SpreadInside)
-    }
-}
 @Composable
 fun MyComplexLayout(modifier: Modifier) {
     Column (
@@ -203,7 +73,7 @@ fun MyComplexLayout(modifier: Modifier) {
                 modifier = Modifier
                     .weight(1f)
                     .fillMaxHeight()
-                    .background(Color.Red),
+                    .background(Color.Blue),
                 contentAlignment = Alignment.Center
             ){
                 Text("Ejemplo 2")
@@ -218,6 +88,7 @@ fun MyComplexLayout(modifier: Modifier) {
                 Text("Ejemplo 3")
             }
         }
+        Spacer(Modifier.height(50.dp))
         Box(
             modifier = Modifier
                 .fillMaxWidth()
@@ -230,61 +101,17 @@ fun MyComplexLayout(modifier: Modifier) {
     }
 }
 @Composable
-fun MyColumn(modifier: Modifier) {
+fun MyStateExample(modifier: Modifier) {
+    var counter by rememberSaveable {mutableStateOf(0)}
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(text = "Ejemplo 1", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(100.dp))
-        Text(text = "Ejemplo 1", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(100.dp))
-        Text(text = "Ejemplo 1", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(100.dp))
-        Text(text = "Ejemplo 1", modifier = Modifier
-            .background(Color.Black)
-            .weight(1f))
-        Text(text = "Ejemplo 1", modifier = Modifier.background(Color.Cyan))
-        Text(text = "Ejemplo 1", modifier = Modifier.background(Color.Blue))
-    }
-}
-
-@Composable
-fun MyRow(modifier: Modifier) {
-    Row(modifier = modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween){
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-        Text("Ejemplo", modifier = Modifier.width(100.dp))
-    }
-}
-
-@Composable
-fun MyBox(name: String, modifier: Modifier = Modifier) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Box(
-            modifier = Modifier
-                .width(50.dp)
-                .height(50.dp)
-                .background(color = Color.Cyan)
-                .verticalScroll(rememberScrollState())
-        ) {
-            Text("Esto es un Ejemplo")
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Button(onClick = { counter++}){
+            Text(text= "Pulsar")
         }
+        Text(text = "He sido pulsaodo ${counter} veces")
     }
 }
 
@@ -292,6 +119,6 @@ fun MyBox(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PracticasJetpackComposeTheme {
-        MyComplexLayout(Modifier)
+        MyStateExample(Modifier)
     }
 }
